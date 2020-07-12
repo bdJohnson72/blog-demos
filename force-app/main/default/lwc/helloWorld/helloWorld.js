efc828fe6da8d64379c2d05a49e55788355fdfde
Brooks Johnson <brooksjohnson157@gmail.com>
2020-07-12 15:35:25 -0400

    commit source code

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bjohnson on 7/5/20.
+ */
+
+import {LightningElement} from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+
+}
---
+++ force-app/main/default/lwc/testComp/testComp.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bjohnson on 6/16/20.
+ */
+
+import {LightningElement} from 'lwc';
+
+export default class TestComp extends LightningElement {
+
+}
